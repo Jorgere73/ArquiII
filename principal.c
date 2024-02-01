d282b2fcbe0f3a7112cd3c2b9f44d4c2a8c7f535
IsmmoranCresp <145661165+IsmmoranCresp@users.noreply.github.com>
2024-02-01 12:56:52 +0000

    todo añadido
    He hecho un par de cambios, me queda comentarlo bonito y poco más, échale un vistazo a ver si te gusta como lo tengo

--- principal.c
+++ principal.c
@@ -4,94 +4,99 @@
 #include <unistd.h>
 #include <ctype.h>
 #include <sys/wait.h>
-#include <sys/types.h>
 #include <signal.h>
 
-int main(int argc, char *argv[])
-{
-    //Comprobamos que se haya pasado un parámetro
-    if(argc > 1)
-    {
-        //Puntero que apuntará a un valor '\0' si el argumento del programa es un número
-        char *fin;
-        //Convertimos argv[1] a long, en base decimal, y si todo correcto: *fin = '\0'
-        //Número de procesos hijo que vamos a tener que crear
-        long numHijos = strtol(argv[1], &fin, 10);
-        //Número de hijos creados hasta que uno falla, para saber cuántos procesos parar en caso de error
+//HAY QUE HACER DE 0 -------------------------ESTA MAL----------------------------------------------------------
+
+int main(int argc, char *argv[]){
+
+
+        /*creo un puntero para usarlo en strtol que me indicara si cuando lo hemos pasado por dicha funcion ha llegado al final (\0) o por si al contrario, apunta a un caracter*/
+        char *fin = NULL;
+
+        /*se crea un string para almacenar la concatenacion de "+" en funcion del numero recibido por parametro en el terminal*/ 
+         
+        /*esta funcion me pasa el numero que paso por parametro a long int y el puntero fin apunta a la ultima direccion de memoria*/
+        long int num_ter=strtol(argv[1], &fin, 10);
         int numHijosCreados = 0;
-        //Comprobamos que el parámetro sea un número, y sea positivo
-        if(numHijos > 0 && (*fin == '\0'))
-        {
-            //Array que guarda los pids de los procesos hijos para envío de señales
-            pid_t pidHijos[numHijos];
-            for (int i = 1; i < numHijos+1; i++) 
-            {
 
-                // Creamos el número de procesos que indica el argumento del programa
-                pid_t pid = fork();
+        
+            if (argv[1] == NULL){
+                /*Si recibimos una cadena vacia, imprimimos error*/
+                printf("no hay argumentos [principal.c]\n");
+            }
+            else{
+
+
                 
-                // Comprobamos que no haya errores al crear los procesos
-                if (pid < 0) 
-                {
-                    perror("Error al crear un proceso hijo");
-                    //Guardamos cuántos procesos se han creado, para pararlos con kill()
-                    numHijosCreados = i;
-                    //Salimos del for()
-                    break;
-                }
+                
+                /*si el numero tecleado por pantalla es mayor que 0, es positivo y por tanto seguimos con el programa ademas hacemos la comprobacion de si contiene un caracter*/
+                if(num_ter > 0 && *fin == '\0' ){
+                    pid_t pid_Hijos[num_ter];
+                    for(int i = 1; i<=num_ter;i++){
 
-                // Disociamos el proceso principal de hijos
-                else if (pid == 0) {
-                    //Guarda la conversión de i a str
-                    char n[5];  
-                    //Convertimos i en str, guardándolo en n
-                    sprintf(n, "%d", i);
-                    //Creamos el array de parámetros para proceso.c
-                    char *args[] = {"./proceso", n, NULL};
-                    //Ejecutamos el programa para el proceso hijo actual
-                    execvp("./proceso", args);
-                    //Si la ejecución del programa falla, devolvemos error
-                    perror("Error al ejecutar el programa");
-                    //Salimos del programa con señal de error
+
+                        pid_t pid;
+                        pid = fork();
+                        
+                        if (pid < 0) 
+                        {
+                            perror("Error al crear un proceso hijo [principal.c]");
+                            //Guardamos cuántos procesos se han creado, para pararlos con kill()
+                            numHijosCreados = i;
+                            //Salimos del for()
+                            break;
+                        }
+
+                        if(pid == 0){
+                            /*no entiendo como funciona el almacenamiento y envío*/
+                            
+                            //char n[10];
+                            char *n = malloc((i+1)*sizeof(char));
+                            sprintf(n, "%d", i);
+                            execl("./proceso","proceso",n, (char *)NULL); 
+
+                        }
+                        else{
+                            //Dentro del proceso padre, guardamos los pids de los procesos hijos
+                            pid_Hijos[i-1] = pid;
+                            //Si no hay errores, numHijosCreados será igual a numHijos
+                            numHijosCreados = i;
+                        }
+                    }
+                    //Si el número de hijos creados fue menor al pasado por parámetro, hubo un error en la creación
+                    if(numHijosCreados < num_ter)
+                    {
+                        //Salimos con señal de error
+                        exit(EXIT_FAILURE);
+                    }
+                    else{
+                    //sleep(5);
+                        for (int i = 0; i < numHijosCreados; i++) {
+                            //Envíamos señal SIGSTOP a cada uno de los procesos hijos y comprobamos si da error
+                            /*kill nos sirve para enviar una señal, en esta caso con SIGSTOP paramos los procesos, pero no los matamos*/
+                            if(kill(pid_Hijos[i], SIGSTOP) == -1)
+                            {
+                                perror("Error al enviar la señal de stop [principal.c]\n");
+                            }
+                            else
+                            {
+                                printf("Proceso hijo %d parado con éxito\n", pid_Hijos[i]);
+                            }
+                        }
+                        //Salimos con señal correcta
+                        exit(EXIT_SUCCESS);
+                    }
+                    
+                }
+                
+                else{
+                    perror("Argumento no es entero o menor que 0 [principal.c]");
                     exit(EXIT_FAILURE);
                 }
-                else
-                {
-                    //Dentro del proceso padre, guardamos los pids de los procesos hijos
-                    pidHijos[i-1] = pid;
-                    //Si no hay errores, numHijosCreados será igual a numHijos
-                    numHijosCreados = i;
-                }
+                
+                
+               
             }
-            //sleep(2);
-            //Recorremos el for() para parar únicamente los procesos iniciados correctamente (numHijosCreados)
-            for (int i = 0; i < numHijosCreados; i++) {
-                //Envíamos señal SIGSTOP a cada uno de los procesos hijos y comprobamos si da error
-                if(kill(pidHijos[i], SIGSTOP) == -1)
-                {
-                    perror("Error al enviar la señal de stop\n");
-                }
-                else
-                {
-                    printf("Proceso hijo %d parado con éxito\n", pidHijos[i]);
-                }
-            }
-            //Si el número de hijos creados fue menor al pasado por parámetro, hubo un error en la creación
-            if(numHijosCreados < numHijos)
-            {
-                //Salimos con señal de error
-                exit(EXIT_FAILURE);
-            }
-            else
-            {
-                //Salimos con señal correcta
-                exit(EXIT_SUCCESS);
-            }
-        }
-    }
-    else
-    {
-        printf("No se han introducido suficientes argumentos\n");
-        exit(EXIT_FAILURE);
-    }
+    
 }
