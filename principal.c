c290621f2f2a5b4ff0cf4fe953312d644a6d8909
Jorgere73 <100472783@alumnos.uc3m.es>
2024-02-01 01:39:29 +0100

    paso 2 hecho

--- principal.c
+++ principal.c
@@ -4,6 +4,8 @@
 #include <unistd.h>
 #include <ctype.h>
 #include <sys/wait.h>
+#include <sys/types.h>
+#include <signal.h>
 
 int main(int argc, char *argv[])
 {
@@ -18,29 +20,63 @@ int main(int argc, char *argv[])
         //Comprobamos que el parámetro sea un número, y sea positivo
         if((atoi(argv[1]) > 0) && (*fin == '\0'))
         {
-            for(int i = 0; i < numHijos; i++)
+            //Array que guarda los pids de los procesos hijos para envío de señales
+            pid_t pidHijos[numHijos];
+            for (int i = 1; i < numHijos+1; i++) 
             {
-                //Creamos el número de procesos que indica el argumento del programa
-                pid_t pid = fork();
 
-                //Comprobamos que no haya errores al crear los procesos
-                if(pid < 0)
+                // Creamos el número de procesos que indica el argumento del programa
+                pid_t pid = fork();
+                
+                // Comprobamos que no haya errores al crear los procesos
+                if (pid < 0) 
                 {
                     perror("Error al crear un proceso hijo");
-                    exit(-1);
+                    //Sale con error
+                    exit(EXIT_FAILURE);
                 }
 
-                //Disociamos el proceso principal de hijos
-                if(pid == 0)
+                // Disociamos el proceso principal de hijos
+                else if (pid == 0) {
+                    //Guarda la conversión de i a str
+                    char n[5];  
+                    //Convertimos i en str, guardándolo en n
+                    sprintf(n, "%d", i);
+                    //Creamos el array de parámetros para proceso.c
+                    char *args[] = {"./proceso", n, NULL};
+                    //Ejecutamos el programa para el proceso hijo actual
+                    execvp("./proceso", args);
+                    //Si la ejecución del programa falla, devolvemos error
+                    perror("Error al ejecutar el programa");
+                    //Salimos del programa con señal de error
+                    exit(EXIT_FAILURE);
+                }
+                else
                 {
-                
+                    //Dentro del proceso padre, guardamos los pids de los procesos hijos
+                    pidHijos[i-1] = pid;
+                    //printf("%d\n", pidHijos[i]);
                 }
+            }
+            //sleep(2);
+            for (int i = 0; i < numHijos; i++) {
+                //Envíamos señal SIGSTOP a cada uno de los procesos hijos y comprobamos si da error
+                if(kill(pidHijos[i], SIGSTOP) == -1)
+                {
+                    perror("Error al enviar la señal de stop\n");
                 }
-            
+                else
+                {
+                    printf("Proceso hijo %d parado con éxito\n", pidHijos[i]);
+                }
+            }
+
+            exit(EXIT_SUCCESS);
         }
     }
     else
     {
         printf("No se han introducido suficientes argumentos\n");
+        exit(EXIT_FAILURE);
     }
 }
