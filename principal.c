936e4d4eade5eee3bbbc11ad18dd10db01ca6446
IsmmoranCresp <145661165+IsmmoranCresp@users.noreply.github.com>
2024-02-03 12:09:35 +0000

    PASO 3 CASI
    falta meterle los de alarm y la señal de alarm de los cujons, si te da toc , mas toc te va a dar que los tengo todos seguido, un saludo

--- principal.c
+++ principal.c
@@ -5,35 +5,41 @@
 #include <ctype.h>
 #include <sys/wait.h>
 #include <signal.h>
+#include <time.h>
+
+#define DEFAULT_QUANTUM 1
 
 
-int main(int argc, char *argv[])
-{
+void alarma(int SIGNUM);
+int main(int argc, char *argv[]){
+
 
         /*creo un puntero para usarlo en strtol que me indicara si cuando lo hemos pasado por dicha funcion ha llegado al final (\0) o por si al contrario, apunta a un caracter*/
         char *fin = NULL;
-        /*creo una variable int, donde almacenare la cantidad de hijos creados para gestionar los errores*/
+        char *fin2 = NULL;
+        time_t quantum = DEFAULT_QUANTUM;
         int numHijosCreados = 0;
-        
         /*esta funcion me pasa el numero que paso por parametro a long int y el puntero fin apunta a la ultima direccion de memoria*/
         long int num_ter=strtol(argv[1], &fin, 10);
 
-        
-            if (argv[1] == NULL)
-            {
+            if(argv[2] != NULL){
+                quantum = strtol(argv[2], &fin2, 10);
+                if(*fin2 != '\0'){
+                    perror("Argumento(s) no es entero o menor que 0 [principal.c]");
+                    exit(EXIT_FAILURE); 
+                }
+            }
+            if (argv[1] == NULL){
                 /*Si recibimos una cadena vacia, imprimimos error*/
                 printf("no hay argumentos [principal.c]\n");
+                exit(EXIT_FAILURE);
             }
-            else
-            {
+            else{
                 /*si el numero tecleado por pantalla es mayor que 0, es positivo y por tanto seguimos con el programa ademas hacemos la comprobacion de si contiene un caracter*/
-                if(num_ter > 0 && *fin == '\0' )
-                {
-                    /*creamos una variable pid_t para poder gestionar los procesos hijos*/            
+                if(num_ter > 0 && *fin == '\0'){
                     pid_t pid_Hijos[num_ter];
-                    for(int i = 1; i<=num_ter;i++)
-                    {
-                        /*creamos los procesos*/    
+                    for(int i = 1; i <= num_ter;i++){
+
                         pid_t pid;
                         pid = fork();
                         
@@ -46,18 +52,14 @@ int main(int argc, char *argv[])
                             break;
                         }
 
-                        if(pid == 0)
-                        {
-                            
+                        if(pid == 0){
+                            /*no entiendo como funciona el almacenamiento y envío*/
                             //char n[10];
                             char *n = malloc((i+1)*sizeof(char));
-                            /*pasamos de int a caracter*/
                             sprintf(n, "%d", i);
-                            /*usamos execl para ejecutar el ejecutable de proceso*/
                             execl("./proceso","proceso",n, (char *)NULL); 
                         }
-                        else
-                        {
+                        else{
                             //Dentro del proceso padre, guardamos los pids de los procesos hijos
                             pid_Hijos[i-1] = pid;
                             //Si no hay errores, numHijosCreados será igual a numHijos
@@ -65,18 +67,15 @@ int main(int argc, char *argv[])
                         }
                     }
 
-                        
                     //Si el número de hijos creados fue menor al pasado por parámetro, hubo un error en la creación
                     if(numHijosCreados < num_ter)
                     {
                         //Salimos con señal de error
                         exit(EXIT_FAILURE);
                     }
-                    else
-                    {
-                    //sleep(5);
-                        for (int i = 0; i < numHijosCreados; i++) 
-                        {
+                    else{
+                    
+                        for (int i = 0; i < numHijosCreados; i++) {
                             //Envíamos señal SIGSTOP a cada uno de los procesos hijos y comprobamos si da error
                             /*kill nos sirve para enviar una señal, en esta caso con SIGSTOP paramos los procesos, pero no los matamos*/
                             if(kill(pid_Hijos[i], SIGSTOP) == -1)
@@ -88,14 +87,46 @@ int main(int argc, char *argv[])
                                 printf("Proceso hijo %d parado con éxito\n", pid_Hijos[i]);
                             }
                         }
+                        
+                        while(1){
+                            alarm(quantum);
+                            for(int i = 0; i < numHijosCreados; i++){
+
+                                    
+                                    if(kill(pid_Hijos[i], SIGCONT) == -1){
+                                        perror("Error al enviar la señal de continuar [principal.c]\n");
+                                    }
+                                    else{
+                                        //kill(pid_Hijos[i], SIGALRM);
+                                        //sleep(quantum);
+                                        
+                                        if(kill(pid_Hijos[i], SIGSTOP) == -1){
+                                            perror("Error al enviar la señal de stop [principal.c]\n");
+                                        }
+                                    }
+                                    
+                            }
+                        }
+                        
                         //Salimos con señal correcta
                         exit(EXIT_SUCCESS);
-                    }   
+                    }
+                    
                 }
-                else
-                {
-                    perror("Argumento no es entero o menor que 0 [principal.c]");
+                
+                else{
+                    perror("Argumento(s) no es entero o menor que 0 [principal.c]");
                     exit(EXIT_FAILURE);
-                }               
+                }
+                
+                
+               
             }
+    
+}
+
+void alarma(int Signum){
+
+    printf("alarma\n");
+    alarm(Signum);
 }
