696cee6a4315d28024fe6f5d14174fad88e7e07f
IsmmoranCresp <145661165+IsmmoranCresp@users.noreply.github.com>
2024-02-06 15:04:38 +0000

    funciona guay

--- principal.c
+++ principal.c
@@ -12,13 +12,13 @@
 #define PERROR(m){perror(m); exit(EXIT_FAILURE);}
 #define PRINTF(men){printf(men); exit(EXIT_FAILURE);}
 
-/*lo declaramos como variable global para poder usarlo en la funcion que usamos signal*/
-time_t quantum = DEFAULT_QUANTUM;
 void alarma(int SIGNUM);
 
 void alarma(int Signum)
 {
     //incrementaremos contador
+    
+
 }
 
 int main(int argc, char *argv[]){
@@ -29,51 +29,56 @@ int main(int argc, char *argv[]){
     int numHijosCreados = 0;
     long int num_hijos = 0;
     char n[10];
-
-    long arg3 = 0;
-    int *tiemposEjec = (int*) malloc(sizeof(int));
+    time_t quantum = DEFAULT_QUANTUM;
+    int comprueba = 0;
+    long tiempos_ejecucion = 0;
+    int *tiemposEjec_Hijos = (int*) malloc(sizeof(int));
     int contadorTiempos = 0;
 
+    /*PROCESADOR DE ARGUMENTOS*/
     if(argc == 1) PRINTF("No hay argumentos que procesar [-n] ó [-q]\n");    
     for(int i = 1; i < argc;)
     {
-        arg3 = strtol(argv[i], &fin, 10);
-        //printf("%d",arg3);
+        tiempos_ejecucion = strtol(argv[i], &fin, 10);
         if(strcmp(argv[i], "-n") == 0) 
         {
-            if (argv[i+1] == NULL) PRINTF("no hay argumentos [principal.c]\n");
+            if (argv[i+1] == NULL) PRINTF("no hay argumentos en [-n] [principal.c]\n");
             /*esta funcion me pasa el numero que paso por parametro a long int y el puntero fin apunta a la ultima direccion de memoria*/
             num_hijos=strtol(argv[i+1], &fin, 10); 
             /*si lo que tecleamos por pantalla es una letra o menor que 0, es negativo y por tanto terminamos el programa*/         
             if(num_hijos < 0 || *fin != '\0') PRINTF("Argumento(s) no es entero o menor que 0 [principal.c]\n"); 
-            tiemposEjec = (int*) realloc(tiemposEjec, sizeof(int) * num_hijos);
+            tiemposEjec_Hijos = (int*) realloc(tiemposEjec_Hijos, sizeof(int) * num_hijos);
+            comprueba = 1;
             i+=2;
         }
         else if(strcmp(argv[i], "-q") == 0)
         {
             if(argv[i+1] == NULL) PRINTF("No hay argumentos que procesar en [-q]\n");
             quantum = strtol(argv[i+1], &fin2, 10);
-            if(*fin2 != '\0' || quantum < 0) PRINTF("Argumento(s) no es entero o menor que 0 [principal.c]\n")
+            if(*fin2 != '\0' || quantum < 0) PRINTF("Argumento(s) no es entero o menor que 0 [principal.c]\n");
             i+=2;
         }
-        else if(*fin == '\0' && arg3 > 0)
+        else if(*fin == '\0' && tiempos_ejecucion > 0)
         {
-            if(contadorTiempos > num_hijos)
+            if(contadorTiempos >= num_hijos)
             {
-                free(tiemposEjec);
-                perror("Demasiados argumentos proporcionados para el número de hijos que hay");
-                exit(EXIT_FAILURE);
-
+                free(tiemposEjec_Hijos);
+                PRINTF("Demasiados argumentos proporcionados para el número de hijos\n");
             } 
-            tiemposEjec[contadorTiempos] = arg3;
-            //printf("%d\n", tiemposEjec[contadorTiempos]);
+            tiemposEjec_Hijos[contadorTiempos] = tiempos_ejecucion;
+            printf("%d\n", tiemposEjec_Hijos[contadorTiempos]);
             contadorTiempos++;
             i++;
         }
-        else PRINTF("Modificador(es) desconocido [-q] [-n]\n"); 
-    }
+        else if(*fin != '\0' || tiempos_ejecucion < 0){
+            PRINTF("tiempo(s) de ejecucion menor que 0 o no es(son) entero(s)\n");
+        }else PRINTF("Modificador(es) desconocido [-q] [-n]\n"); 
+    }   
+    /*comprobamos que se haya introducido el argumento -n*/
+    if(comprueba != 1){ free(tiemposEjec_Hijos); PRINTF("Es obligatorio el argumento [-n]\n");}
+    /*AQUI FINALIZA EL PROCESADOR DE ARGUMENTOS POR TERMINAL*/
     
-    
+    /*COMIENZO DE  LA FUNCIONALIDAD DEL PROGRAMA*/
     signal(SIGALRM, alarma);
     pid_t pid_Hijos[num_hijos];
     for(int i = 1; i <= num_hijos;i++)
@@ -81,14 +86,7 @@ int main(int argc, char *argv[]){
         pid_t pid;
         pid = fork();
                     
-        if (pid < 0) 
-        {
-            perror("Error al crear un proceso hijo [principal.c]\n");
-            //Guardamos cuántos procesos se han creado, para pararlos con kill()
-            numHijosCreados = i;
-            //Salimos del for()
-            break;
-        }
+        if (pid < 0) PERROR("Error al crear un proceso hijo [principal.c]\n");
         if(pid == 0)
         {
             sprintf(n, "%d", i);
@@ -104,18 +102,20 @@ int main(int argc, char *argv[]){
     }
 
     //Si el número de hijos creados fue menor al pasado por parámetro, hubo un error en la creación
-    if(numHijosCreados < num_hijos) PERROR("Error en creacion, numero de hijos menor que parametro escrito en terminal [principal.c]\n")
+    if(numHijosCreados < num_hijos) PERROR("Error en creacion, numero de hijos menor que parametro escrito en terminal [principal.c]\n");
     for (int i = 0; i < numHijosCreados; i++)
     {
         //Envíamos señal SIGSTOP a cada uno de los procesos hijos y comprobamos si da error
         /*kill nos sirve para enviar una señal, en esta caso con SIGSTOP paramos los procesos, pero no los matamos*/
         if(kill(pid_Hijos[i], SIGSTOP) == -1) PERROR("Error al enviar la señal de stop [principal.c]\n");
     }
+
     while(1)
     {
         for(int i = 0; i < numHijosCreados; i++)
         {                                    
             if(kill(pid_Hijos[i], SIGCONT) == -1) PERROR("Error al enviar la señal de continuar [principal.c]\n");
+            alarm(tiemposEjec_Hijos[i]);
             alarm(quantum);
             /*pausamos el proceso hasta que recibamos una señal*/
             pause();      
